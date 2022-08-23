b106af81f0cde86938a992b771e7a555a9535034
ellagoesbeyond <108352420+ellagoesbeyond@users.noreply.github.com>
2022-08-23 10:55:06 +0200

    changes

--- week 3/week_three_day_1_LAB_SQL_Queries_Elisabeth_O.sql
+++ week 3/week_three_day_1_LAB_SQL_Queries_Elisabeth_O.sql
@@ -20,7 +20,7 @@ order by account_id asc limit 5;
 select account_id from loan where duration=60 order by amount asc limit 5;
 # What are the unique values of k_symbol in the order table?
 # Note: There shouldn't be a table name order, since order is reserved from the ORDER BY clause. You have to use backticks to escape the order table name.
-select distinct k_symbol from `order`;
+select distinct k_symbol from `order` order by k_symbol desc;
 # In the order table, what are the order_ids of the client with the account_id 34?
 select order_id from `order`where account_id = 34;
 # In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)?
@@ -38,15 +38,15 @@ select trans_id, `date`, `type` , amount from trans where account_id = 793 order
 #13
 # In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? 
 # Show the results sorted by the district_id in ascending order.
-select district_id, count(district_id ) from client where district_id<10 group by district_id order by district_id asc;
+select district_id, count(district_id) from client where district_id<10 group by district_id order by district_id asc;
 
 # 14
 # the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
 select type, count(card_id) from card group by type order by count(card_id) desc;
 
-#15 ##DOES NOT WORK
+#15 
 #Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts.
-# select account_id, sum(amount) from loan  order by sum(amount) asc  limit 10;
+select account_id, sum(amount) from loan  group by account_id order by sum(amount) desc  limit 10;
 
 #16 
 #In the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in descending order.
@@ -55,13 +55,14 @@ select date, count(loan_id) from loan where date < 930907 group by date  order b
 # 17 
 # In the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration,
 # ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
-select  date, duration,count(amount) from loan where date between 971201 and 971231 group by date, duration order by date , duration;
+select  date, duration,count(loan_id) from loan where date between 971201 and 971231 group by date, duration order by date , duration;
 
 
 #18
 #In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). 
 #Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type.
 select account_id, type, sum(amount) as total_amount from trans where account_id = 396 group by type, account_id order by type ;
+
 
 #19 ### not able to change values forr transaction type 
 #From the previous output, translate the values for type to English, rename the column to transaction_type, round total_amount down to an integer
